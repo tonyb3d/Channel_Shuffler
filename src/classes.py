9b3d2eb26ce9bb525900a0199a121db10310461e
Tony <tony.baldin@gmail.com>
2022-11-22 10:30:40 +0300

    Create classes.py
    Черновик класса материала

---
+++ src/classes.py
@@ -0,0 +1,43 @@
+from loguru import logger
+import pathlib
+
+class Material():
+    def __init__(self):
+        super().__init__()
+        
+        self.name = ()
+        self.albedo = ()
+        self.metallic = ()
+        self.roughness = ()
+        self.ao = ()
+        self.normal = ()
+        self.height = ()
+
+
+    def set_name(self):
+        pass
+
+    def set_albedo(self):
+        pass
+
+    def set_metallic(self):
+        pass
+
+    def set_roughness(self):
+        pass
+
+    def set_ao(self):
+        pass
+
+    def set_normal(self):
+        pass
+
+    def set_height(self):
+        pass
+
+    def set_export_path(self):
+        pass
+
+    
+
+    
